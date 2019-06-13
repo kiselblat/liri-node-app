76e88161d39de4deba5d46412cd74b90ec3d3e9b
Tom <tsechrist@outlook.com>
2019-06-13 14:44:32 -0500

    Adds interface user can now search spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+var axios = require('axios');
+
 require('dotenv').config();
 
 var keys = require('./keys.js');
@@ -5,13 +7,38 @@ var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(JSON.stringify(response.tracks.items[0].name , null , 2));
-    console.log(JSON.stringify(response.tracks.items[0].artists[0].name , null , 2));
-    console.log(JSON.stringify(response.tracks.items[0].external_urls.spotify , null , 2));
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+var command = process.argv[2];
+// console.log(command);
+var argument = process.argv.slice(3).join(" ");
+// console.log(argument);
+
+var spotifyThisSong = function(searchString) {
+  spotify
+    .search({ type: 'track', query: searchString })
+    .then(function(response) {
+      var track = response.tracks.items[0];
+      console.log(track.name);
+      console.log(track.artists[0].name);
+      console.log(track.album.name);
+      console.log(track.external_urls.spotify);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+switch(command) {
+  case 'concert-this':
+    //concertThis(argument);
+    break;
+  case 'spotify-this-song':
+    if (!argument) {
+      spotifyThisSong("The Sign Ace of Base");
+    } else {
+      spotifyThisSong(argument);
+    }
+    break;
+  case 'movie-this':
+    //movieThis(argument);
+    break;
+}
