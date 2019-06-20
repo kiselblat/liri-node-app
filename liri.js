5d0ffadb6f494c8eeb5034a897a5ab0d23b20321
Tom <tsechrist@outlook.com>
2019-06-19 22:17:54 -0500

    adds improvements to console.log and log.txt out adds input from command line to log.txt

--- liri.js
+++ liri.js
@@ -12,57 +12,71 @@ var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+// display variable
+var divider = "--------------------------------------------------------------------------------";
+
 // Read in the command and argumend passed from the command line
 var command = process.argv[2];
-var argument = process.argv.slice(3).join(" ");
+var argument = process.argv.slice(3).join("%20");
 // console.log(command , argument);
 
 // Displays a given band's touring schedule, as provided by bandsintown
 var concertThis = function(searchString) {
-  var queryUrl = "https://rest.bandsintown.com/artists/" + searchString + "/events?app_id=codingbootcamp"
-  // console.log(`concertThis(${searchString})` , queryUrl);
+  var queryUrl = `https://rest.bandsintown.com/artists/${searchString}/events?app_id=codingbootcamp`
+  // console.log(`concertThis(${searchString}) , ${queryUrl}`);
+  fs.appendFile("log.txt" , `\nconcertThis(${searchString}): ${queryUrl}\n` , errorHandler);
+
   axios.get(queryUrl).then(
     function(response) {
-      fs.appendFile("log.txt" , `***${searchString} Tour Dates***\n` , errorHandler);
+
+      var searchTitle = `\n${searchString} Tour Dates\n${divider}`
+      
+      console.log(searchTitle);
+      fs.appendFile("log.txt" , `${searchTitle}` , errorHandler);
 
       response.data.forEach(function(element) {
-        var venueName = element.venue.name;
-        var venuePlace = "";
-        if (element.venue.region) {
-          venuePlace = (`${element.venue.city}, ${element.venue.region}`);
-        } else {
-          venuePlace = (`${element.venue.city}, ${element.venue.country}`);
+        // bands in town returns undefined to .region when venue is outside of US.
+        var venuePlace = function() {
+          if (element.venue.region) {
+            return `${element.venue.city}, ${element.venue.region}`;
+          } else {
+            return `${element.venue.city}, ${element.venue.country}`;
+          }
         }
-        var concertTime = moment(element.datetime).format("MM/DD/YYYY");
 
-        console.log(venueName);
-        console.log(venuePlace);
-        console.log(concertTime);
-        console.log('--------------------------------');
+        var concertData = [
+          `Venue: ${element.venue.name}`,
+          `Location: ${venuePlace()}`,
+          `Date: ${moment(element.datetime).format("MM/DD/YYYY")}`,
+        ].join('\n');
 
-        fs.appendFile("log.txt" , `${venueName}\n${venuePlace}\n${concertTime}\n----------------\n` , errorHandler);
+        console.log(`${concertData}\n${divider}`);
+
+        fs.appendFile("log.txt" , `\n${concertData}\n${divider}` , errorHandler);
       });
+      console.log(`\n${response.data.length} results\n`);
+      fs.appendFile("log.txt" , `\n${response.data.length} results\n` , errorHandler);
     })
     .catch(errorHandler);
 }
 
 // Displays information about a song whose title is provided by the user, provided by Spotify
 var spotifyThisSong = function(searchString) {
+  fs.appendFile("log.txt" , `\nspotifyThisSong(${searchString})\n` , errorHandler);
   spotify
     .search({ type: 'track', query: searchString })
     .then(function(response) {
       var track = response.tracks.items[0];
-      var trackName = track.name;
-      var trackArtist = track.artists[0].name;
-      var trackAlbum = track.album.name;
-      var trackUrl = track.external_urls.spotify;
+      var trackData = [
+        `Title: ${track.name}`,
+        `Artist: ${track.artists[0].name}`,
+        `Album: ${track.album.name}`,
+        `URL: ${track.external_urls.spotify}`
+      ].join('\n');
 
-      console.log(trackName);
-      console.log(trackArtist);
-      console.log(trackAlbum);
-      console.log(trackUrl);
+      console.log(`\n${trackData}\n${divider}`);
 
-      fs.appendFile("log.txt" , `${trackName}\n${trackArtist}\n${trackAlbum}\n${trackUrl}\n----------------\n` , errorHandler);
+      fs.appendFile("log.txt" , `\n${trackData}\n${divider}` , errorHandler);
     })
     .catch(errorHandler);
 }
@@ -70,40 +84,45 @@ var spotifyThisSong = function(searchString) {
 // Displays information about a movie whose title is provided by the user, provided by OMDB
 var movieThis = function(searchString) {
   var queryUrl = "http://www.omdbapi.com/?t=" + searchString + "&y=&plot=short&apikey=trilogy";
-  // console.log("movieThis(): ", searchString , queryUrl);
+  // console.log(`movieThis(): ${searchString} , ${queryUrl}`);
+  fs.appendFile("log.txt" , `\nmovieThis(): ${searchString} , ${queryUrl}\n` , errorHandler);
+
   axios.get(queryUrl).then(
     function(response) {
-      
       var movieRatings = "";
       response.data.Ratings.forEach(function(element) {
-        movieRatings += `${element.Source}: ${element.Value} `;
+        movieRatings += `${element.Source}: ${element.Value}\n         `;
       });
 
       var movieData = [
-        "Title: " + `${response.data.Title} (${response.data.Year})`,
-        "Starring: " + response.data.Actors,
-        "Summary: " + response.data.Plot,
-        "Ratings: " + movieRatings,
-        `Country: ${response.data.Country} Language(s): ${response.data.Language}`  
+        `${response.data.Title} (${response.data.Year})\n`,
+        `Starring: ${response.data.Actors}\n`,
+        `Summary: ${response.data.Plot}\n`,
+        `Ratings: ${movieRatings}`,
+        `Country: ${response.data.Country}\n`,
+        `Language(s): ${response.data.Language}`  
       ].join('\n');
 
-      console.log(movieData);
+      console.log(`\n${movieData}\n${divider}`);
 
-      fs.appendFile("log.txt" , `${movieData}\n----------------\n` , errorHandler);
+      fs.appendFile("log.txt" , `\n${movieData}\n${divider}` , errorHandler);
     })
     .catch(errorHandler);
-}
-
-// Takes a command and argument from a seperate file
-var doWhatItSays = function() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    errorHandler(error);
-    var fileInput = data.split(',' , 2);
-    var instruction = fileInput[0];
-    var searchString = fileInput[1].slice(1 , fileInput[1].length - 1);
-    console.log("doWhatItSays(): " , instruction , searchString);
-    initiate(instruction , searchString);
-  });
+  }
+  
+  // Takes a command and argument from a seperate file
+  var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      errorHandler(error);
+      
+      var fileInput = data.split(',' , 2);
+      var instruction = fileInput[0];
+      // either axios.get or omdb and bandsintown doesn't like quotes around the string
+      var searchString = fileInput[1].slice(1 , fileInput[1].length - 1);
+      // console.log(`doWhatItSays():  ${instruction} , ${searchString}`);
+      fs.appendFile("log.txt" , `\ndoWhatItSays():  ${instruction} , ${searchString}\n` , errorHandler);
+      initiate(instruction , searchString);
+    });
 }
 
 // DRY out all the anonymous error handlers
@@ -118,21 +137,21 @@ var initiate = function(doThis , search) {
   switch(doThis) {
     case 'concert-this':
       if (typeof search === typeof undefined || search === "") {
-        concertThis("Hootie and the Blowfish");
+        concertThis("Hootie%20and%20the%20Blowfish");
       } else {
         concertThis(search);
       }
       break;
     case 'spotify-this-song':
       if (typeof search === typeof undefined || search === "") {
-        spotifyThisSong("The Sign Ace of Base");
+        spotifyThisSong("The%20Sign%20Ace%20of%20Base");
       } else {
         spotifyThisSong(search);
       }
       break;
     case 'movie-this':
       if (typeof search === typeof undefined || search === "") {
-        movieThis("Mr. Nobody");
+        movieThis("Mr.%20Nobody");
       } else {
         movieThis(search);
       }
